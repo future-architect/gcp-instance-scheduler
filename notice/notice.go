451a10099af52e02924341242aa94e824fa1c35e
donkomura <koiru3822fs@gmail.com>
2019-07-25 16:29:44 +0900

    Fix: use structure field name as index

--- notice/notice.go
+++ notice/notice.go
@@ -3,6 +3,7 @@ package notice
 import (
 	"fmt"
 	"time"
+	"reflect"
 
 	"github.com/future-architect/gcp-instance-scheduler/model"
 	"github.com/nlopes/slack"
@@ -18,32 +19,62 @@ func getDate() string {
 	return fmt.Sprintf("%d/%d/%d", year, month, day)
 }
 
-func createHeader(pad [4]int) string {
+func createHeader(pad map[string]int) string {
 	text := fmt.Sprintf("Instances Shutdown Report <%s>\n", getDate())
-	text += fmt.Sprintf("%*s | %*s | %*s | %*s\n",
-		pad[0], "InstanceType",
-		pad[1], "Done Shutdown",
-		pad[2], "Already Shutdowned",
-		pad[3], "Skipped")
+
+	for i, fieldName := range getFieldNameList(&model.ShutdownReport{}) {
+		text += fmt.Sprintf("%*v", pad[fieldName], fieldName)
+		if i != 0 {
+			text += " | "
+		}
+	}
+	text += "\n"
+
+//	// to get status name
+//	statusName := reflect.ValueOf(&model.ShutdownReport{}).Type()
+//
+//	for i := 1; i < statusName.NumField() - 1; i++ {
+//		status := statusName.Field(i).Name
+//		text += fmt.Sprintf("%*s | ", pad[status], status)
+//	}
+//	lastStatus := statusName.Field(statusName.NumField()-1).Name
+//	text += fmt.Sprintf("%s\n", pad[lastStatus], lastStatus)
+
 	text += fmt.Sprintf("-----------------------------------------------------------------\n")
 	return text
 }
 
 func createDetailReport(r *model.ShutdownReport) string {
 	var text string
-	pad := -18
+	pad := -25
 
-	for _, d := range r.DoneResources {
-		text += fmt.Sprintf("%*s | %s\n", pad, "DoneResource", d)
-	}
-	for _, a := range r.AlreadyShutdownResources {
-		text += fmt.Sprintf("%*s | %s\n", pad, "AlreadyShutdownResource", a)
-	}
-	for _, s := range r.SkipResources {
-		text += fmt.Sprintf("%*s | %s\n", pad, "SkipResources", s)
+	// fiels values of model.ShutdownReport
+	resultList := reflect.ValueOf(r)
+	// field names of model.ShutdownReport
+	statusName := resultList.Type()
+
+	text += fmt.Sprintf("%s\n", statusName.Field(0).Name)
+
+	for i := 1; i < statusName.NumField(); i++ {
+		status := statusName.Field(i).Name
+		for _, resource := range resultList.FieldByName(status).Interface().([]string) {
+			text += fmt.Sprintf("%*s | %s\n", pad, status, resource)
+		}
 	}
 
 	return text
+}
+
+// return struct 
+func getFieldNameList(i interface{}) []string {
+	var fieldName []string
+
+	fieldTypes := reflect.ValueOf(i).Type()
+	for i := 0; i < fieldTypes.NumField(); i++ {
+		fieldName = append(fieldName, fieldTypes.Field(i).Name)
+	}
+
+	return fieldName
 }
 
 func NewSlackNotifier(slackAPIToken, slackChannel string) *slackNotifier {
@@ -74,17 +105,27 @@ func (n *slackNotifier) postThreadInline(text, ts string) error {
 
 // return timestamp to make thread bellow this message
 func (n *slackNotifier) PostReport(report []*model.ShutdownReport) (string, error) {
-	pad := [4]int{-15, -15, -18, -15}
+	pad := map[string]int{
+		"InstanceType": -15,
+		"DoneResources": -15,
+		"AlreadyShutdownResources": -25,
+		"SkipResources": -15,
+	}
 
 	text := createHeader(pad)
 
 	for _, execResult := range report {
 		sum := execResult.CountResource()
-		text += fmt.Sprintf("%*s | %*d | %*d | %*d\n",
-			pad[0], execResult.InstanceType,
-			pad[1], sum[model.Done],
-			pad[2], sum[model.Already],
-			pad[3], sum[model.Skip])
+		statusList := getFieldNameList(&model.ShutdownReport{})
+		for i := 0; i < len(statusList); i++ {
+			status := statusList[i]
+			if i == 0 {
+				text += fmt.Sprintf("%*s | ", pad[status], status)
+			} else {
+				text += fmt.Sprintf(" %*d", pad[status], sum[i])
+			}
+		}
+		text += "\n"
 	}
 
 	text += fmt.Sprintf("-----------------------------------------------------------------\n")
